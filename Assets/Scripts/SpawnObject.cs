9d88c06bc6c407fd70faf98d01803e3876e6ddb3
lucascz37 <la0359813@gmail.com>
2020-05-13 20:38:37 -0300

    Commit Inicial

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _prefab;
+    private ARRaycastManager _gerenciadorRayCast;
+    private Vector2 _posicaoToque;
+
+    static List<ARRaycastHit> acertos = new List<ARRaycastHit>();
+    private void Awake()
+    {
+        _gerenciadorRayCast = GetComponent<ARRaycastManager>();
+    }
+
+    bool PegarPosicaoToque(out Vector2 _posicaoToque)
+    {
+        if(Input.touchCount > 0)
+        {
+            _posicaoToque = Input.GetTouch(0).position;
+            return true;
+        }
+        _posicaoToque = default;
+        return false;
+    }
+
+    private void Update()
+    {
+        if(!PegarPosicaoToque(out Vector2 _posicaoToque))
+        {
+            return;
+        }
+        if(_gerenciadorRayCast.Raycast(_posicaoToque, acertos, TrackableType.PlaneWithinPolygon))
+        {
+            var lugar = acertos[0].pose;
+            Instantiate(_prefab, lugar.position, lugar.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/VrMode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrMode : MonoBehaviour
+{
+    public void ModoVR()
+    {
+        StartCoroutine(ActivateVR());
+    }
+    IEnumerator ActivateVR()
+    {
+        string device = "cardboard";
+        if(string.Compare(UnityEngine.XR.XRSettings.loadedDeviceName, device, true) != 0)
+        {
+            UnityEngine.XR.XRSettings.LoadDeviceByName(device);
+            yield return null;
+        }
+
+        UnityEngine.XR.XRSettings.enabled = true;
+    }
+    // Update is called once per frame
+
+}
